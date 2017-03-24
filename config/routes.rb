b2bc5d35461452ecb702d0c5a86b1a28d576f71e
ninooo29 <kazuki@ninomiya-no-MacBook-Pro.local>
2017-03-24 21:18:03 +0900

    markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tops#index"
+  resources :users do
+    resources :messages, only: [:index, :create]
+  end
 
 end
