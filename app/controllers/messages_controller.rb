41a2ada71dd8ace9ad00c2bbebe499ac6d832db7
ninooo29 <kazuki@ninomiya-no-MacBook-Pro.local>
2017-03-29 09:44:10 +0900

    create message index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,22 +5,15 @@ class MessagesController < ApplicationController
   end
 
   def new
-    # binding.pry
-    if params[:user_id].present?
-      # user_groups = Group_user.find_by_sql('select * from users where user_id = #{current_user.id}')
-      # receive_user_groupsoup_user.find_by_sql('select * from users where user_id = #{params[:user_id}')
-      # if user_groups & receive_user_groups
-      # if current_user.group_users.where(user_id: params[:user_id]).blank?
-      groups_one = current_user.groups
-      groups_two = User.find(params[:user_id]).groups
-      unless (groups_one & groups_two).present?
-        group = current_user.groups.create(name: "", status: :individual)
-        group.group_users.create(user_id: params[:user_id])
-      end
+    groups_one = current_user.groups
+    groups_two = User.find(params[:user_id]).groups
+    unless (groups_one & groups_two).present?
+      group = current_user.groups.create(name: "", status: :individual)
+      group.group_users.create(user_id: params[:user_id])
+      @group = group.id
+    else
+      @group = (groups_one & groups_two).first.id
     end
-
-    # @group = Group.group_users(user_id: params[:user_id])
-
     @messages = Message.where(user_id: params[:user_id],login_user_id: current_user.id )
     @partner = User.find(params[:user_id])
     @message = Message.new
@@ -33,7 +26,7 @@ class MessagesController < ApplicationController
 
   private
   def messages_params
-    params.require(:message).permit(:body, :image).merge(login_user_id: current_user.id, user_id: params[:user_id])
+    params.require(:message).permit(:body, :image, :group_id).merge(login_user_id: current_user.id, user_id: params[:user_id])
   end
 
 end
